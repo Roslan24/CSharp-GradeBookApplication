36903c501a465ea8323c84cf0c74827e420dc0da
Roslan Arslanouk <a.roslan24@gmail.com>
2019-03-12 01:03:43 -0400

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,4 @@
 ﻿using GradeBook.Enums;
-using System;
-using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -16,7 +14,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
-            var threshold = (int)Math.Ceiling(Students.Count*0.2);
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (grades[threshold - 1] <= averageGrade)
@@ -29,6 +27,32 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else
+                base.CalculateStatistics();
+
+            return;
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else
+                base.CalculateStudentStatistics(name);
+
+            return;
         }
     }
 }
